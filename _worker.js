51abb8d56ad06d582c41129699eb3aa5e8cea668
longking2012 <ypwddyp8jk@privaterelay.appleid.com>
2026-05-07 18:44:35 +0800

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -1,16 +1,7 @@
 export default {
   async fetch(request) {
-    const url = new URL(request.url);
-
-    // 搜索功能
-    if (url.pathname === "/search") {
-      const q = url.searchParams.get("q") || "";
-      return Response.redirect("https://www.google.com/search?q=" + encodeURIComponent(q), 302);
-    }
-
-    // 默认打开谷歌主页
     return fetch("https://www.google.com", {
-      headers: { Host: "www.google.com" },
+      headers: { Host: "www.google.com" }
     });
   }
 };
